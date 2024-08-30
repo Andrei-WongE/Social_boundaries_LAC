49f7c8233a071ec33bdd12d22cf3a58810c9c231
Andrei-WongE <awonge01@student.bbk.ac.uk>
2024-08-30 15:59:39 +0100

    Added more data, lasts plot dont work

--- LSOA_example.R
+++ LSOA_example.R
@@ -21,16 +21,22 @@ pkgs = c("dplyr", "tidyverse", "janitor", "sf"
          , "tmap", "devtools", "renv", "Hmisc", "ggplot2"
          , "xfun", "remotes", "sp", "spdep", "maptools"
          , "foreach", "doParallel", "parallel", "progress"
-         , "doSNOW", "purrr", "patchwork", "stringi","rmapshaper"
-         , "dplyr", "openxlsx", "MASS"
+         , "doSNOW", "purrr", "patchwork", "rmapshaper"
+         , "dplyr", "openxlsx", "MASS", "kaggler"
          )
 
-groundhog.library(pkgs, groundhog.day, force.source.main = TRUE)
+groundhog.library(pkgs, groundhog.day)
+
+devtools::install_github("koderkow/kaggler")
+require(kaggler)
+
 # groundhog.library("stringi", groundhog.day, force.source.main = TRUE)
+##ISSUES
+# With Matrix and stringi, copy folder from the library to the groundhog_library
 
 packageVersion("openxlsx")
 # [1] ‘4.2.5.2’
-# renv::snapshot(exclude = c("INLA", "socialFrontiers")
+# renv::snapshot(exclude = c("INLA", "socialFrontiers"))
 
 # Get LSOA boundaries:----
 # from: https://data.london.gov.uk/dataset/statistical-gis-boundary-files-london
@@ -168,79 +174,93 @@ lsoa_data_sf <- lsoa_data_sf %>%
                 , "car_or_van_availability_no_cars_or_vans_in_household_percent_2011"
               )
 
-# Adding other sources of data, checl NOMIS
+# Adding other sources of data, check NOMIS
 dir(here("Data", "London"), pattern = ".csv")
 # [1] "employment_pop.csv"             
 # [2] "local_units_indus-employ.csv"  MSOA!!! 
 # [3] "LSOA_Atlas.csv"                 
 # [4] "Qualification_by_age_LSOA11.csv"
 
-url <- "https://www.nomisweb.co.uk/api/v01/dataset/NM_141_1.data.xlsx?geography=1245708449...1245708476,1245708289,1245708620...1245708645,1245715064,1245715067,1245708646...1245708705,1245714941,1245708822...1245708865,1245708886...1245708919,1245714947,1245708920...1245708952,1245714930,1245714931,1245714944,1245708978...1245709014,1245709066...1245709097,1245714948,1245709121...1245709150,1245714999,1245715000,1245709179...1245709239,1245708290...1245708310,1245714945,1245708311...1245708378,1245714932,1245708379...1245708448,1245714929,1245714934,1245714936,1245708477...1245708519,1245714935,1245708520...1245708557,1245714938,1245708558...1245708592,1245714940,1245708593...1245708619,1245714933,1245715072...1245715076,1245708706...1245708733,1245714942,1245715028,1245708734...1245708794,1245714943,1245708795...1245708821,1245714939,1245708866...1245708885,1245708953...1245708977,1245709015...1245709042,1245714946,1245715069,1245715070,1245709043...1245709065,1245709098...1245709120,1245714982,1245709151...1245709178&date=latestMINUS12&industry=163577857...163577874&employment_sizeband=0,10,20,30,40&legal_status=0,10&measures=20100&signature=NPK-e156dc8ba10b7412aa8bed:0x6ee9910ec10c11565ad435bf27a42320737b893c"
-##Content
-# 2
-# 2011, Total, Total, value
-# 3
-# 2011, Total, Private sector total, value
-# 4
-# 2011, Micro (0 to 9), Total, value
-# 5
-# 2011, Micro (0 to 9), Private sector total, value
-# 6
-# 2011, Small (10 to 49), Total, value
-# 7
-# 2011, Small (10 to 49), Private sector total, value
-# 8
-# 2011, Medium-sized (50 to 249), Total, value
-# 9
-# 2011, Medium-sized (50 to 249), Private sector total, value
-# 10
-# 2011, Large (250+), Total, value
-# 11
-# 2011, Large (250+), Private sector total, value
-
-local_units <- read.xlsx(  url
-                         , sheet = 3
-                         , startRow = 2
-                         , skipEmptyRows = TRUE
-                         , skipEmptyCols = TRUE
-                         # , rows = c(2:10138)
-                         # , cols = c(1:20)
-                 )
-
-View(local_units)
-
-# # Function to detect blank rows
-# is_blank_row <- function(row) {
-#   all(row == "")
-# }
 # 
-# # Identify the start and end rows of each table
-# table_starts <- which(apply(local_units, 1, is_blank_row) == FALSE)
-# table_ends <- c(table_starts[-1] - 1, nrow(local_units))
+# url <- "https://www.nomisweb.co.uk/api/v01/dataset/NM_141_1.data.xlsx?geography=1245708449...1245708476,1245708289,1245708620...1245708645,1245715064,1245715067,1245708646...1245708705,1245714941,1245708822...1245708865,1245708886...1245708919,1245714947,1245708920...1245708952,1245714930,1245714931,1245714944,1245708978...1245709014,1245709066...1245709097,1245714948,1245709121...1245709150,1245714999,1245715000,1245709179...1245709239,1245708290...1245708310,1245714945,1245708311...1245708378,1245714932,1245708379...1245708448,1245714929,1245714934,1245714936,1245708477...1245708519,1245714935,1245708520...1245708557,1245714938,1245708558...1245708592,1245714940,1245708593...1245708619,1245714933,1245715072...1245715076,1245708706...1245708733,1245714942,1245715028,1245708734...1245708794,1245714943,1245708795...1245708821,1245714939,1245708866...1245708885,1245708953...1245708977,1245709015...1245709042,1245714946,1245715069,1245715070,1245709043...1245709065,1245709098...1245709120,1245714982,1245709151...1245709178&date=latestMINUS12&industry=163577857...163577874&employment_sizeband=0,10,20,30,40&legal_status=0,10&measures=20100&signature=NPK-e156dc8ba10b7412aa8bed:0x6ee9910ec10c11565ad435bf27a42320737b893c"
+# ##Content
+# # 2
+# # 2011, Total, Total, value
+# # 3
+# # 2011, Total, Private sector total, value
+# # 4
+# # 2011, Micro (0 to 9), Total, value
+# # 5
+# # 2011, Micro (0 to 9), Private sector total, value
+# # 6
+# # 2011, Small (10 to 49), Total, value
+# # 7
+# # 2011, Small (10 to 49), Private sector total, value
+# # 8
+# # 2011, Medium-sized (50 to 249), Total, value
+# # 9
+# # 2011, Medium-sized (50 to 249), Private sector total, value
+# # 10
+# # 2011, Large (250+), Total, value
+# # 11
+# # 2011, Large (250+), Private sector total, value
 # 
-# # Extract each table
-# tables <- list()
-# for (i in seq_along(table_starts)) {
-#   tables[[i]] <- data[table_starts[i]:table_ends[i], ]
-# }
+# local_units <- read.xlsx(  url
+#                          , sheet = 3
+#                          , startRow = 2
+#                          , skipEmptyRows = TRUE
+#                          , skipEmptyCols = TRUE
+#                          # , rows = c(2:10138)
+#                          # , cols = c(1:20)
+#                  )
 # 
-# # Print the extracted tables
-# for (i in seq_along(tables)) {
-#   print(tables[[i]])
-# }
+# View(local_units)
+# 
+# # # Function to detect blank rows
+# # is_blank_row <- function(row) {
+# #   all(row == "")
+# # }
+# # 
+# # # Identify the start and end rows of each table
+# # table_starts <- which(apply(local_units, 1, is_blank_row) == FALSE)
+# # table_ends <- c(table_starts[-1] - 1, nrow(local_units))
+# # 
+# # # Extract each table
+# # tables <- list()
+# # for (i in seq_along(table_starts)) {
+# #   tables[[i]] <- data[table_starts[i]:table_ends[i], ]
+# # }
+# # 
+# # # Print the extracted tables
+# # for (i in seq_along(tables)) {
+# #   print(tables[[i]])
+# # }
+# 
+# 
+# lsoa_data_sf <- merge(lsoa_data, local_units, by.x = "LSOA11CD", by.y = "LSOA11CD")
+# 
+# dim(lsoa_data_sf)
+# # [1] 4829   38
+# 
+# saveRDS(lsoa_data_sf, here("Data","London", "lsoa_data_sf.rds"))
 
+# Adding other sources of data, check Kaagle
+# https://www.kaggle.com/datasets/dalreada/all-uk-active-companies-by-sic-and-geolocated
+# Set-up account: https://koderkow.github.io/kaggler/articles/kaggler.html
 
-lsoa_data_sf <- merge(lsoa_data, local_units, by.x = "LSOA11CD", by.y = "LSOA11CD")
+# Authenticate with Kaggle
+kgl_auth()
 
-dim(lsoa_data_sf)
-# [1] 4829   38
+# Download dataset (replace with actual dataset info)
+kgl_datasets_download_all("microsoft/ms-malware-prediction")
+
+# Unzip the downloaded file
+unzip("ms-malware-prediction.zip")
+
 
 saveRDS(lsoa_data_sf, here("Data","London", "lsoa_data_sf.rds"))
-
 lsoa_data_sf <- readRDS(here("Data","London", "lsoa_data_sf.rds"))
 
-
-                
 #Census data https://www.nomisweb.co.uk/census/2011/bulk
 # Job density https://www.nomisweb.co.uk/datasets/jd
 # Query https://www.nomisweb.co.uk/query/select/getdatasetbytheme.asp?theme=75&subgrp=Local+Characteristics
@@ -250,12 +270,12 @@ lsoa_data_sf <- readRDS(here("Data","London", "lsoa_data_sf.rds"))
 
 # Boundaries detection (Following Legewie, 2018) -----
 
-library("MASS")
 # library("matrixStats")
-library("scales")
+require("scales")
+require("spdep")
 
 groundhog.library(c("tidycensus", "stargazer", "lintr",
-                    "styler", "matrixStats", "stringi"
+                    "styler", "matrixStats"
 )
 , groundhog.day
 # , ignore.deps = "Matrix"
@@ -270,11 +290,50 @@ source(here("BoundaryDetection.R"))
 # Difference variables
 colnames(lsoa_data_sf)
 
-vars <- c("POB_SUB", "POB_INF")
+# [14] "ethnic_group_white_percent_2011"                                                                              
+# [15] "ethnic_group_mixed_multiple_ethnic_groups_percent_2011"                                                       
+# [16] "ethnic_group_asian_asian_british_percent_2011"                                                                
+# [17] "ethnic_group_black_african_caribbean_black_british_percent_2011"                                              
+# [18] "ethnic_group_other_ethnic_group_percent_2011"                                                                 
+# [19] "ethnic_group_bame_percent_2011" 
+
+new_names <- c("p_race_white"
+               , "p_race_mixed"
+               , "p_race_asian_brit"
+               , "p_race_african_carib_black"
+               , "p_race_bame"
+              )
+
+old_names <- c("ethnic_group_white_percent_2011"
+                 , "ethnic_group_mixed_multiple_ethnic_groups_percent_2011"
+                 , "ethnic_group_asian_asian_british_percent_2011"
+                 , "ethnic_group_black_african_caribbean_black_british_percent_2011"
+                 , "ethnic_group_bame_percent_2011"
+              )   
+
+lsoa_data_sf <- lsoa_data_sf %>% 
+                dplyr::rename_with(~ new_names, all_of(old_names)) %>% 
+                dplyr::mutate(across(all_of(new_names), ~ . / 100))
+
+vars <- c("p_race_white"
+          , "p_race_mixed"
+          , "p_race_asian_brit"
+          , "p_race_african_carib_black"
+          , "p_race_bame"
+        )
+
+table(new_names)
+
+# Verifying if proportion data is correct
+filtered_data <- lsoa_data_sf %>%
+  filter(if_any(all_of(vars), all_vars(. >= 1)))
+
+View(filtered_data[vars])
+rm(filtered_data)
 
 # #sp do not support empty geometries
-# sum(st_is_empty(chi_ct))
-# # [1] 1
+sum(st_is_empty(lsoa_data_sf))
+# [1] 0
 # chi_ct <- chi_ct[!st_is_empty(chi_ct), ]
 
 
@@ -297,7 +356,7 @@ vars <- c("POB_SUB", "POB_INF")
 #'
 # debug(areal_wombling)
 
-bdr <- areal_wombling(  chi_ct
+  bdr <- areal_wombling(  lsoa_data_sf
                         , vars
                         , threshold = NA #{NA}= fuzzy wombling, {0,1}= crips wombling
 ) 
@@ -316,11 +375,11 @@ par(mfrow = c(1, 2), mar = c(0, 0, 0, 0))
 
 # Plot 1: Areal units with proportion African-American
 scale_color <- col_numeric(c("#F1EEF6", "#034E7B"), domain = c(0, 1))
-plot(as(chi_ct, "Spatial"),
-     col = scale_color(chi_ct$p_race_black),
+plot(as(lsoa_data_sf, "Spatial"),
+     col = scale_color(lsoa_data_sf$p_race_african_carib_black),
      lwd = 0.01, bor = "white"
 )
-vals <- quantile(chi_ct$p_race_black, probs = c(0, 0.25, .5, 0.75, 1), na.rm = TRUE)
+vals <- quantile(lsoa_data_sf$p_race_african_carib_black, probs = c(0, 0.25, .5, 0.75, 1), na.rm = TRUE)
 legend(-87.87, 41.73,
        legend = c("0%", "25%", "50%", "75%", "100%"),
        fill = scale_color(vals), cex = 0.8,
@@ -328,9 +387,9 @@ legend(-87.87, 41.73,
 )
 
 # Plot 2: Boundaries
-plot(bdr, lwd = rescale(bdr$p_race_black_blv, to = c(0.1, 1.25)))
+plot(bdr, lwd = rescale(bdr$p_race_african_carib_black_blv, to = c(0.1, 1.25)))
 # The spatial lines object does not include Chicago's city boundaries. Let's add them
-chi <- st_union(chi_ct)
+chi <- st_union(lsoa_data_sf)
 plot(as(chi, "Spatial"), lwd = 0.5, add = TRUE)
 # Legend showing the scale of boundary values
 legend(-87.87, 41.73,
@@ -340,16 +399,23 @@ legend(-87.87, 41.73,
        title = "Boundary Value", title.adj = 3.5
 )
 
+png(here("Figures","p_race_african_carib_black_blv.png"), width = 800, height = 600)
+
 # aggregate from line segments to block group level
-chi_ct_blv <- bind_rows(
+lsoa_data_sf_blv <- bind_rows(
   group_by(bdr@data, i) %>%
     summarise_at(vars(ends_with("blv")), max, na.rm = TRUE),
   group_by(bdr@data, j) %>%
     summarise_at(vars(ends_with("blv")), max, na.rm = TRUE) %>% dplyr::rename(i = j)
 ) %>%
   group_by(i) %>%
-  summarise_at(vars(ends_with("blv")), max, na.rm = TRUE)
-chi_ct <- bind_cols(chi_ct, select(chi_ct_blv, -i))
+  summarise_at(vars(ends_with("blv")), max, na.rm = TRUE) %>% 
+  janitor::clean_names()
+
+lsoa_data_sf_boundaries <- bind_cols(lsoa_data_sf
+                                     , dplyr::select(lsoa_data_sf_blv
+                                     , -i)
+                                    )
 
 # compare the boundary values for spatial lines (border line segments) with the
 # boundary values for areal units.
@@ -357,31 +423,47 @@ chi_ct <- bind_cols(chi_ct, select(chi_ct_blv, -i))
 par(mfrow = c(1, 2), mar = c(0, 0, 0, 0))
 
 # Plot 1: Boundary values for border line segments
-plot(bdr, lwd = rescale(bdr$p_race_black_blv, to = c(0.1, 1.25)))
-chi <- st_union(chi_ct)
+(summary_data <- bdr@data %>%
+  summarise(mean_value = mean(p_race_african_carib_black_blv)
+            , min_value = min(p_race_african_carib_black_blv)
+            , max_value = max(p_race_african_carib_black_blv)
+          )
+)
+
+# plot(bdr, lwd = rescale(bdr$p_race_african_carib_black_blv, to = c(0.1, 1.25)))
+plot(bdr, lwd = rescale(bdr$p_race_african_carib_black_blv, to = c(0.1, 0.35)))
+# plot(bdr, lwd = bdr$p_race_african_carib_black_blv)
+
+chi <- st_union(lsoa_data_sf_boundaries)
+
 plot(as(chi, "Spatial"), lwd = 0.5, add = TRUE)
 legend(-87.87, 41.73,
        legend = c("0.0", "0.25", "0.5", "0.75", "1.0"),
-       lwd = rescale(c(0, 0.25, 0.5, 0.75, 1), to = c(0.1, 1.25)),
+       lwd = rescale(c(0, 0.25, 0.5, 0.75, 1), to = c(0.1, 0.35)),
+       # lwd = rescale(c(0, 0.25, 0.5, 0.75, 1), to = c(0.1, 1.25)),
        cex = 0.8, box.lty = 0, border = "#00000000",
        title = "Boundary Value", title.adj = 3.5
 )
 
+png(here("Figures","Boundary_values_segment.png"), width = 800, height = 600)
+
 # Plot 2: Boundary values for border line segments for areal units
-sel <- is.finite(chi_ct$p_race_black_blv)
+sel <- is.finite(lsoa_data_sf_boundaries$p_race_african_carib_black_blv)
 scale_color <- col_numeric(c("#F1EEF6", "#034E7B"), domain = c(0, 1))
-cols <- scale_color(chi_ct$p_race_black_blv[sel])
-plot(as(chi_ct[sel, ], "Spatial"), lwd = 0.1, col = cols, bor = "white")
+cols <- scale_color(lsoa_data_sf_boundaries$p_race_african_carib_black_blv[sel])
+
+plot(as(lsoa_data_sf_boundaries[sel, ], "Spatial"), lwd = 0.1, col = cols, bor = "white")
 legend(-87.87, 41.73,
        legend = c("0.0", "0.25", "0.5", "0.75", "1.0"),
        fill = scale_color(c(0, 0.25, 0.5, 0.75, 1)), cex = 0.8, box.lty = 0,
        border = "#00000000", title = "Boundary Value", title.adj = 3.5
 )
 
+png(here("Figures","Boundary_values_areal.png"), width = 800, height = 600)
 
 
 # Spatial regression ################################################
 f1 <- "crime_violent ~ edge_wombling_race + log(pop) + p_race_black + p_race_hisp +
 p_race_asian + con_disadv + res_instab + immi_con + hhi + age_15_35_male"
-m1 <- glm.nb(f1, data = chi_ct)
+m1 <- glm.nb(f1, data = lsoa_data_sf)
 stargazer(m1, type = "text", no.space = TRUE, star.cutoffs = c(0.05, 0.01, 0.001))
